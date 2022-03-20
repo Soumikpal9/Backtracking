757e2c53f0a78e8a742d8ebcdbb18836f1559aca
Pal <soumik.pal@capgemini.com>
2022-03-20 13:00:18 +0530

    Unique Permutations

---
+++ UniquePermutations.cpp
@@ -0,0 +1,92 @@
+/*
+
+        Problem Description
+
+        Given an array A of size N denoting collection of numbers that might contain duplicates, return all possible unique permutations.
+
+        NOTE: No 2 entries in the permutation sequence should be the same.
+
+        WARNING: DO NOT USE LIBRARY FUNCTION FOR GENERATING PERMUTATIONS. Example : next_permutations in C++ / itertools.permutations in python.
+        If you do, we will disqualify your submission retroactively and give you penalty points.
+
+
+        Problem Constraints
+
+        1 <= |A| <= 9
+
+        0 <= A[i] <= 10
+
+
+
+        Input Format
+
+        Only argument is an integer array A of size N.
+
+
+
+        Output Format
+
+        Return a 2-D array denoting all possible unique permutation of the array.
+
+
+
+        Example Input
+
+        Input 1:
+
+        A = [1, 1, 2]
+        Input 2:
+
+        A = [1, 2]
+
+
+        Example Output
+
+        Output 1:
+
+        [ [1,1,2]
+        [1,2,1]
+        [2,1,1] ]
+        Output 2:
+
+        [ [1, 2]
+        [2, 1] ]
+
+
+        Example Explanation
+
+        Explanation 1:
+
+        All the possible unique permutation of array [1, 1, 2].
+        Explanation 2:
+
+        All the possible unique permutation of array [1, 2].
+
+*/
+
+void getPermutations(int n, unordered_map<int, int> &freq, vector<int> &temp, vector<vector<int>> &ans){
+    if(temp.size()==n){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=0;i<=10;i++){
+        if(freq[i]>0){
+            freq[i]--;
+            temp.push_back(i);
+            getPermutations(n, freq, temp, ans);
+            freq[i]++;
+            temp.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> Solution::solve(vector<int> &A){
+    vector<vector<int>> ans;
+    vector<int> temp;
+    unordered_map<int, int> freq;
+    for(int i=0;i<A.size();i++){
+        freq[A[i]]++;
+    }
+    getPermutations(A.size(), freq, temp, ans);
+    return ans;
+}
